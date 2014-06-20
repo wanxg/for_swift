84aae9232c896cb273f4d2e645f15b3461ed4b6c
wanxg <wanxg@wanxgdeimac.local>
2014-06-20 22:01:43 +0200

    done

--- SnakesAndLadders/main.swift
+++ SnakesAndLadders/main.swift
@@ -8,5 +8,86 @@
 
 import Foundation
 
-println("Hello, World!")
+
+var snakeRules = [24:16, 22:20, 19:8, 14:4]
+
+var ladderRules = [3:11, 6:17, 9:18, 10:12]
+
+var keys = Array(snakeRules.keys) + Array(ladderRules.keys)
+
+
+func checkRule(indices:Int) -> Int {
+    
+    var result: Int? = indices
+    
+    for key in keys {
+        
+        if key == indices {
+            
+            var snakeResult = snakeRules[indices]
+            
+            if snakeResult != nil {
+                
+                print("BAD! YOU HIT SNAKE! MOVING BACK TO ")
+            } else {
+                
+                print("NICE! YOU HIT LADDER! MOVING FORWARD TO ")
+            }
+            
+            result =  snakeResult != nil ? snakeResult : ladderRules[indices]
+            
+            println(result)
+            
+            break
+        }
+    }
+
+    return result!;
+}
+
+func rollDice() -> Int {
+    
+    return Int(arc4random_uniform(6)) + 1
+
+}
+
+println("WELCOME TO SNAKES AND LADDERS!")
+
+var currentSquare = 1
+
+while (currentSquare < 25) {
+    
+    println("--------------------------------")
+    
+    println("YOU STANDING ON SQUARE \(currentSquare)")
+    
+    print("ROLLING DICE ... GOT ")
+    
+    var dice = rollDice()
+    
+    println(dice)
+    
+    currentSquare += dice
+    
+    if currentSquare >= 25 {
+        
+        break
+    }
+    
+    println("MOVING TO \(currentSquare)")
+    
+    var result = checkRule(currentSquare)
+
+    if result != dice {
+
+      currentSquare = result
+    }
+    
+
+}
+
+println("CONGRUDULATION! GAME OVER!")
+
+
+
 
