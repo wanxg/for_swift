22a9dec417f559aad80a1cc6561cbde1011a44fe
wanxg <wanxg@wanxgdeimac.local>
2014-06-20 10:17:09 +0200

    Initial Commit

---
+++ SnakesAndLadders/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SnakesAndLadders
+//
+//  Created by wanxg on 14-6-20.
+//  Copyright (c) 2014年 wanxg. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
